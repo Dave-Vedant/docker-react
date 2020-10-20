2cd85a5e3d0ffe5eb1ead7f6b14edb03fbe86747
dave117(Vedant) <vedantdave77@gmail.com>
2020-10-20 18:41:54 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           "HI There!, Please God Help Me!"
+          "I want your help, now!, this 2020 is really horrible!"
         </p>
         <a
           className="App-link"
